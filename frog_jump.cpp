73c551da0f1ae147357defad04ca6e7c9c844e2c
shiying <37130120+yyyshi@users.noreply.github.com>
2020-12-28 17:17:16 +0800

    Create frog_jump.cpp

---
+++ frog_jump.cpp
@@ -0,0 +1,33 @@
+//dp
+int FrogJump(int n) {
+	std::vector<int> dp(n + 1);
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	return dp[n];
+}
+
+// 
+int FrogJump(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+
+	if (n == 2)
+	{
+		return 2;
+	}
+
+	return FrogJump(n - 1) + FrogJump(n - 2);
+}
+
+void main()
+{
+	int res = FrogJump(7);
+	_ASSERTE(res == 21);
+}
