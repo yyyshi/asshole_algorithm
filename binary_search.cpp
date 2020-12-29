088ac56ff9d754f0aa0b75651c6f13cd3d7b2b0f
shiying <37130120+yyyshi@users.noreply.github.com>
2020-12-29 20:07:37 +0800

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,69 @@
+// iteration func
+int binarySearchIteration(int array[], int start, int end, int value)
+{
+	int left = start;
+	int right = end;
+
+	// pos
+	int mid;
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		// find it
+		if (value == array[mid])
+		{
+			return mid;
+		}
+		// keep left part
+		else if (value < array[mid])
+		{
+			right = mid - 1;
+		}
+		// keep right part
+		else if (value > array[mid])
+		{
+			left = mid + 1;
+		}
+	}
+
+	// not found
+	return -1;
+}
+
+int binarySearchRecursive(int arr[], int left, int right, int value)
+{
+	int mid = left + (right - left) / 2;
+	// not found
+	if (left > right)
+	{
+		return -1;
+	}
+
+	// find it
+	if (arr[mid] == value)
+	{
+		return mid;
+	}
+	// keep left part
+	else if (arr[mid] > value)
+	{
+		return binarySearchRecursive(arr, left, mid - 1, value);
+	}
+	// keep right part
+	else
+	{
+		return binarySearchRecursive(arr, mid + 1, right, value);
+	}
+}
+
+// simple test
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int pos = binarySearchRecursive(arr, 0, 9, 4);
+	pos = pos;
+
+	int pos2 = binarySearchIteration(arr, 0, 9, 4);
+	pos2 = pos2;
+  return 1;
+}
