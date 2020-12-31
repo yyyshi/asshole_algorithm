3e5a76dffc4920d684ee0b4ab77ac387fce50dbe
shiying <37130120+yyyshi@users.noreply.github.com>
2020-12-31 16:35:57 +0800

    Create top_k.cpp

---
+++ top_k.cpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <queue>
+
+// binary heap is complete binary tree
+namespace ns_little_heap
+{
+  // use STL::priority_queue
+	void little_heap()
+	{
+		auto vec = {1, 2, 3, 4 ,5 ,6};
+		std::priority_queue<int> heap;
+		for (auto i : vec)
+		{
+			heap.push(i);
+		}
+
+
+		auto newVec = {10 , 7, 13};
+		for (auto i : newVec)
+		{
+			heap.push(i);
+		}
+
+		while (!heap.empty())
+		{
+			std::cout << heap.top() << std::endl;
+			heap.pop();
+		}
+	}
+  
+  // todo(shiying)
+};
