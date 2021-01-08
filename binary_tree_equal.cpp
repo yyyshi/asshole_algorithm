345f065409fce3e21d872714c7de03c1d8452487
shiying <37130120+yyyshi@users.noreply.github.com>
2021-01-08 14:55:14 +0800

    Create binary_tree_equal.cpp

---
+++ binary_tree_equal.cpp
@@ -0,0 +1,57 @@
+namespace ns_binary_tree_equal
+{
+	bool IfBinaryTreeEqual(TreeNode* root1, TreeNode* root2)
+	{
+		if (!root1 && !root2)
+		{
+			return true;
+		}
+
+		if (!root1 || !root2)
+		{
+			return false;
+		}
+
+		if (root1->data == root2->data)
+		{
+			if (IfBinaryTreeEqual(root1->left, root2->left)
+				&& IfBinaryTreeEqual(root1->right, root2->right)
+				|| IfBinaryTreeEqual(root1->left, root2->right)
+				&& IfBinaryTreeEqual(root1->right, root2->left))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	// somple test
+	void IfBinaryTreeEqual_test()
+	{
+		// tree1
+		//     0
+		//   /   \
+		//  1     2
+		TreeNode* p0 = new TreeNode(0);
+		TreeNode* p1 = new TreeNode(1);
+		TreeNode* p2 = new TreeNode(2);
+		p0->left = p1;
+		p0->right = p2;
+
+		// tree2
+		//    0
+		//  /   \
+		//  1
+		//   \
+		//    2
+		TreeNode* s0 = new TreeNode(0);
+		TreeNode* s1 = new TreeNode(1);
+		TreeNode* s2 = new TreeNode(2);
+		s0->left = s1;
+		s1->right = s2;
+		// cmp
+		bool equal = IfBinaryTreeEqual(p0, s0);   // false
+		equal = equal;
+	}
+};
