7b7638284a6211f5f0a524d5c643ebff6025276f
shiying <37130120+yyyshi@users.noreply.github.com>
2021-01-10 14:36:04 +0800

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -1,115 +1,75 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-
-namespace ns_string
+namespace ns_my_string
 {
 	class String
 	{
 	public:
-		String(const char *str = nullptr)
-		{
-			if (!str)
-			{
-				length = 0;
-				data = new char[1];
-				*data = '\0';
-			}
-			else
-			{
-				data = new char[strlen(str) + 1];
-				strcpy(data, str);
-			}
-		}
+		String();
+		String(const char* data);
+		String(const String& rhs);
+		String& operator=(const String& rhs);
+		friend std::ostream& operator<<(std::ostream& os, String& rhs);
+		char* c_str();
+		size_t size();
 
-		String(const String &str)
-		{
-			if (data)
-			{
-				delete[] data;
-				length = 0;
-			}
-			length = str.size();
-			data = new char[length + 1];
-			strcpy(data, str.c_str());
-		}
-
-		~String()
-		{
-			length = 0;
-			if (data)
-			{
-				delete[] data;
-			}
-		}
-
-		String operator+(const String &rhs)
-		{
-			return (*this += rhs);
-		}
-
-		String& operator=(const String &rhs)
-		{
-			String tmp(rhs);
-			std::swap(data, tmp.data);
-			return *this;
-		}
-
-		String& operator+=(const String &rhs)
-		{
-			int len = strlen(data) + strlen(rhs.data) + 1;
-			char *newstr = new char[len];
-			memset(newstr, 0, len);
-			strcpy(newstr, data);
-			strcat(newstr, rhs.data);
-
-			delete[] data;
-
-			data = newstr;
-			return *this;
-		}
-		bool operator==(const String &rhs) const
-		{
-			return (rhs.size() == length && strcmp(data, rhs.c_str()) == 0);
-		}
-
-		// access
-		char& operator[](int index) const
-		{
-			return data[index];
-		}
-		size_t size() const
-		{
-			return length;
-		}
-		const char* c_str() const
-		{
-			return data;
-		}
-
-		//friend std::istream& operator >> (std::istream &is, String &rhs)
-		//{
-		// todo(yshi)
-		//}
-		friend std::ostream& operator<<(std::ostream &os, String &rhs)
-		{
-			os << rhs.data;
-			return os;
-		}
+		~String();
 
 	private:
-		char*	data;
-		size_t	length;
+		char* m_data;
 	};
+
+	String::String() : m_data(new char[1])
+	{
+		*m_data = '\0';
+	}
+
+	String::String(const char *str) : m_data(new char[strlen(str) + 1])
+	{
+		strcpy(m_data, str);
+	}
+
+	String::String(const String& rhs) : m_data(new char[strlen(rhs.m_data) + 1])
+	{
+		strcpy(m_data, rhs.m_data);
+	}
+
+	String& String::operator=(const String& rhs)
+	{
+		String tmp(rhs);
+		std::swap(tmp.m_data, this->m_data);
+		return *this;
+	}
+
+	// no need to specified like String::operator<<
+	std::ostream& operator<<(std::ostream& os, String& rhs)
+	{
+		os << rhs.m_data;
+		return os;
+	}
+
+	char* String::c_str()
+	{
+		return m_data;
+	}
+
+	size_t String::size()
+	{
+		return strlen(m_data);
+	}
+
+	String::~String()
+	{
+		delete[] m_data;
+	}
 
 	// simple test
 	void String_test()
 	{
-		String s0 = "hello world";
-		// String s1;
-		String s2 = s0;
-		// s1 = s0;
+		String s0;							//  String() {}
+		String s1 = "hello world";					//  String(const char* str) {}
+		String s2 = s1;							//  String(const String& rhs) {} 
+		s0 = s1;							//  operator=(const String&rhs) {}
 
-		std::cout << s2 << std::endl;
+		std::cout << "s0 = " << s0 << '\t' << "s1 = " << s1 << '\t' << "s2 = " << s2 << '\t' << std::endl;
+		while (1) {}
 	}
 };
